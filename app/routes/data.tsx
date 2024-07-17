f433c1de4f3a52acc0a86a48662b40f2ff373d70
kit39644 <krisanon.nea@rmutto.ac.th>
2024-07-17 19:48:43 +0700

    Week2

---
+++ app/routes/data.tsx
@@ -0,0 +1,33 @@
+export const cards = [
+{
+    id: 100,
+    name: "teerawat barnvimol",
+    bio: "นักศึกษาประจำสาขาวิชาเทคโนโลยีสารสนเทส",
+    bgProd:"/images/image1.png",
+    userIcon:"/images/image3.jpg",
+    userName:"teerawat",
+    createdAt: "Junly 11,2024",
+    active : true
+},
+{
+    id: 102,
+    name: "boss si",
+    bio: "นักศึกษาประจำสาขาวิชาเทคโนโลยีสารสนเทส",
+    bgProd:"/images/image2.jpg",
+    userIcon:"/images/image1.png",
+    userName:"boss si",
+    createdAt: "Junly 10,2024",
+    active : false
+},
+{
+    id: 103,
+    name: "GoFree",
+    bio: "นักศึกษาประจำสาขาวิชาเทคโนโลยีสารสนเทส",
+    bgProd:"/images/image3.jpg",
+    userIcon:"/images/image2.jpg",
+    userName:"#GOFREE",
+    createdAt: "Junly 09,2024",
+    active: true
+},
+
+];
